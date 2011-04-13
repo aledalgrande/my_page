aaaac7f7a9215357f4b6789c0cee84c209b6f5b4
aledalgrande <aledalgrande@gmail.com>
2011-04-14 00:13:18 +0100

    layout with images for details

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,23 @@
 class HomeController < ApplicationController
+  
+  def index
+    @competencies = { :ruby => 'Ruby 1.8.x/1.9.x',
+                      :rails => 'Rails 2.x/3.x',
+                      :rspec => 'RSpec',
+                      :pair => 'Pair Programming',
+                      :jquery => 'jQuery',
+                      :tdd => 'TDD',
+                      :html => 'HTML',
+                      :css => 'CSS',
+                      :ci => 'Continuous Integration',
+                      :agile => 'Agile',
+                      :sinatra => 'Sinatra',
+                      :cucumber => 'Cucumber',
+                      :php => 'PHP 5.2.x/5.3.x',
+                      :phpagi => 'PHPAgi',
+                      :java => 'Java 5/6',
+                      :mac => 'Mac',
+                      :linux => 'Linux' }
+  end
+  
 end
